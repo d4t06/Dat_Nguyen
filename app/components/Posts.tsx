461e4ffb0ce0f2a1e3a29b294addbe8f483c9f2a
datnh <huudat01234560@gmail.com>
2023-07-15 21:06:17 +0700

    update

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -7,7 +7,7 @@ async function Posts() {
 
    // console.log("check postsMeta", postsMeta)
 
-   if (!postsMeta) return <p className="mt-10 text-white text-center">No posts available.</p>
+   if (!postsMeta) return <p className="mt-10 text-white text-center">No posts available.{process.env.GITHUB_TOKEN}</p>
 
    return (
       <div className="w-2/4 mx-auto mt-10">

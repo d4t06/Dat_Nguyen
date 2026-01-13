d8ea1e3a489caa35c0a2f4d66d12b022811e8075
d4t06 <huudat01234560@gmail.com>
2026-01-14 01:05:11 +0700

    update 15/1/2026

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
 	return (
 		<div className="flex-grow flex items-center justify-center">
 			<img height={128} width={128} src="/images/simon.png" />
-			<p className="ml-2 text-xl">"Not found"</p>
+			<p className="ml-2 text-xl">&quot;Not found&quot;</p>
 		</div>
 	);
 }
